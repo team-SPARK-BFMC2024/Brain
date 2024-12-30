df12924b117c55433058cf6b895160752f227ca6
Tan Tai <148557564+Tantai25@users.noreply.github.com>
2024-12-30 14:21:24 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,116 @@
+# To start the project: 
+#
+#       sudo apt update
+#       sudo apt upgrade
+#       xargs sudo apt install -y < "requirement.txt" 
+#       cd src/dashboard/frontend/
+#       curl -fsSL https://fnm.vercel.app/install | bash
+#       source ~/.bashrc
+#       fnm install --lts
+#       npm install -g @angular/cli@17
+#       npm install
+#       if needed: npm audit fix
+#
+# ===================================== GENERAL IMPORTS ==================================
+import sys
+import subprocess
+
+sys.path.append(".")
+from multiprocessing import Queue, Event
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+# ===================================== PROCESS IMPORTS ==================================
+
+from src.gateway.processGateway import processGateway
+from src.dashboard.processDashboard import processDashboard
+from src.hardware.camera.processCamera import processCamera
+from src.hardware.serialhandler.processSerialHandler import processSerialHandler
+from src.data.Semaphores.Semaphores import processSemaphores
+from src.data.TrafficCommunication.processTrafficCommunication import processTrafficCommunication
+from src.utils.ipManager.IpReplacement import IPManager
+# ------ New component imports starts here ------#
+from src.hardware.Autonomous.processAutonomous import ProcessAutonomous
+# ------ New component imports ends here ------#
+# ======================================== SETTING UP ====================================
+allProcesses = list()
+
+queueList = {
+    "Critical": Queue(),
+    "Warning": Queue(),
+    "General": Queue(),
+    "Config": Queue(),
+}
+
+logging = logging.getLogger()
+
+Dashboard = True
+Camera = True
+Semaphores = True
+TrafficCommunication = True
+SerialHandler = True
+
+# ------ New component flags starts here ------#
+Autonomous = True
+# ------ New component flags ends here ------#
+
+# ===================================== SETUP PROCESSES ==================================
+
+# Initializing gateway
+processGateway = processGateway(queueList, logging)
+processGateway.start()
+
+# Ip replacement
+path = './src/dashboard/frontend/src/app/webSocket/web-socket.service.ts'
+IpChanger = IPManager(path)
+IpChanger.replace_ip_in_file()
+
+
+# Initializing dashboard
+if Dashboard:
+    processDashboard = processDashboard( queueList, logging, debugging = False)
+    allProcesses.append(processDashboard)
+
+# Initializing camera
+if Camera:
+    processCamera = processCamera(queueList, logging , debugging = False)
+    allProcesses.append(processCamera)
+
+# Initializing semaphores
+if Semaphores:
+    processSemaphores = processSemaphores(queueList, logging, debugging = False)
+    allProcesses.append(processSemaphores)
+
+# Initializing GPS
+if TrafficCommunication:
+    processTrafficCommunication = processTrafficCommunication(queueList, logging, 3, debugging = False)
+    allProcesses.append(processTrafficCommunication)
+
+# Initializing serial connection NUCLEO - > PI
+if SerialHandler:
+    processSerialHandler = processSerialHandler(queueList, logging, debugging = True)
+    allProcesses.append(processSerialHandler)
+
+# ------ New component runs starts here ------#
+if Autonomous:
+    processAutonomous = ProcessAutonomous(queueList, logging, debugging=False)
+    allProcesses.append(processAutonomous)
+# ------ New component runs ends here ------#
+
+# ===================================== START PROCESSES ==================================
+for process in allProcesses:
+    process.daemon = True
+    process.start()
+
+# ===================================== STAYING ALIVE ====================================
+blocker = Event()
+try:
+    blocker.wait()
+except KeyboardInterrupt:
+    print("\nCatching a KeyboardInterruption exception! Shutdown all processes.\n")
+    for proc in reversed(allProcesses):
+        print("Process stopped", proc)
+        proc.stop()
+
+processGateway.stop()
